b9a906d526956e961ac20b4b94ad8b614eba3ff1
herethere04 <aquilabarbos@sempreceub.com>
2025-11-20 23:12:53 -0300

    Atualiza URL da API para Producao no Azure

--- frontend/app.js
+++ frontend/app.js
@@ -1,5 +1,5 @@
 const appContainer = document.getElementById('app');
-const API_BASE_URL = 'http://localhost:8080/api';
+const API_BASE_URL = 'https://app-sistema-ponto-api-ab.nicesky-09580c7d.brazilsouth.azurecontainerapps.io/api';
 
 // --- FUNÇÃO: DECODIFICADOR DE JWT ---
 function parseJwt(token) {
